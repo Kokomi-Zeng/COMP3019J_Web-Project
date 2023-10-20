806653fbac6a8f6f43692ef251e6febcc1a97ea0
Kokomi Zeng <2845728164@qq.com>
2023-10-20 15:25:51 +0800

    Update seller.py

--- blueprints/seller.py
+++ blueprints/seller.py
@@ -1,50 +1,57 @@
 from flask import Blueprint, render_template, session, request, jsonify
 from werkzeug.security import generate_password_hash
-
+from models import Product, Comment, Buyer, User, Seller
 from exts import db
 
+"""
+The following code is used to store the routes related to sellers,
+such as sellerInfo, getSellerByItemID, getIntroductionByCommentID, modifySellerInfo.
+"""
 seller_bp = Blueprint('seller', __name__, url_prefix='/seller')
 
-from models import Product, Comment, Buyer, User, Seller
 
+# Provide get sellerInfo method for the front end sellerInfo page
 @seller_bp.route('/sellerInfo', methods=['GET'])
 def seller_info():
     phone = request.args.get('phone')
 
-    # 假如没有传入phone，返回服务器200状态码，表示请求成功，但是没有数据
+    # If phone is not passed in, return an empty string for phone
     if not phone:
         return jsonify({"success": False, "message": "Phone number is required"})
 
     seller = Seller.query.filter_by(phone=phone).first()
-    # 假如用户不存在，返回服务器200状态码，表示请求成功，但是没有数据
+
+    # If seller is not found, return an empty string for phone
     if not seller:
         return jsonify({"success": False, "message": "Seller not found"})
 
     return jsonify({
         "phone": seller.phone,
-        # 事实上，这里不能返回密码，因为密码是加密的，除非不让加密，但这不合理，建议直接不显示得了
-        # "password": seller.user.password,
         "name": seller.name,
         "introduction": seller.description,
     })
 
+
+# Provide another get sellerInfo method for use itemID to get sellerInfo
 @seller_bp.route('/getSellerByItemID', methods=['GET'])
 def get_seller_by_item_id():
     product_id = request.args.get('product_id')
 
-    # 验证 product_id 是否存在,而且是int类型
+    # Verify that product_id exists and is an int type
     try:
         product_id = int(product_id)
     except (TypeError, ValueError):
         return jsonify({"success": False, "message": "Invalid Product ID. Please provide a valid integer."})
 
     product = Product.query.filter_by(product_id=product_id).first()
-    # 判断产品是否存在
+
+    # Verify that the product exists
     if not product:
         return jsonify({"success": False, "message": "Product not found"})
 
     seller = product.seller
-    # 判断卖家是否存在
+
+    # Judge whether the seller exists
     if not seller:
         return jsonify({"success": False, "message": "Seller not found"})
 
@@ -55,23 +62,7 @@ def get_seller_by_item_id():
     })
 
 
-@seller_bp.route('/getIntroductionByCommentID', methods=['GET'])
-def get_introduction_by_comment_id():
-    comment_id = request.args.get('comment_id')
-
-    # 验证 comment_id 是否存在,而且是int类型
-    try:
-        comment_id = int(comment_id)
-    except (TypeError, ValueError):
-        return jsonify({"success": False, "message": "Invalid Comment ID. Please provide a valid integer."})
-
-    comment = Comment.query.filter_by(comment_id=comment_id).first()
-    # 判断评论是否存在
-    if not comment:
-        return jsonify({"success": False, "message": "Comment not found"})
-
-
-
+# Provide a method to modify seller information
 @seller_bp.route('/modifySellerInfo', methods=['POST'])
 def modify_seller_info():
     phone = request.json.get('phone')
@@ -79,79 +70,26 @@ def modify_seller_info():
     introduction = request.json.get('introduction')
     password = request.json.get('password')
 
-    # 验证 phone 是否存在
+    # Verify that phone exists
     if not phone:
         return jsonify({"success": False, "message": "Phone number is required"})
 
     seller = Seller.query.filter_by(phone=phone).first()
-    # 判断卖家是否存在
+
+    # Determine whether the seller exists
     if not seller:
         return jsonify({"success": False, "message": "Seller not found"})
 
-    # 更新卖家信息
+    # Update seller information
     if name:
         seller.name = name
     if introduction:
         seller.description = introduction
 
     if password:
-        # 加密
+        # encrypt password
         hashed_password = generate_password_hash(password)
         seller.user.password = hashed_password
 
     db.session.commit()
-
     return jsonify({"success": True, "message": "Seller information updated successfully"})
-
-
-def get_user_info():
-    phone = session.get('phone')
-
-    # 先判断session中是否有phone，如果没有，返回None
-    if not phone:
-        return {
-            # 'phone': None,
-            # 'name': None,
-            'phone': "",
-            'name': "",
-        }
-
-    # 如果有phone，但是数据库中没有该用户，返回None
-    user = User.query.get(phone)
-    if not user:
-        return {
-            # 'phone': None,
-            # 'name': None,
-            'phone': "",
-            'name': "",
-        }
-
-    # 根据用户类型获取姓名, 里面的if判断是为了防止数据库中没有该用户的信息，导致程序报错
-    if user.user_type == '0':  # 卖家
-        if user.seller:
-            name = user.seller.name
-        else:
-            # name = None
-            name = ""
-    elif user.user_type == '1':  # 买家
-        if user.buyer:
-            # name = user.buyer.name
-            name = ""
-        else:
-            # name = None
-            name = ""
-    else:
-        # name = None
-        name = ""
-
-    # 判断name是否为空字符串(是个游客或者卖家)
-    if name == "":
-        return {
-            'phone': "",
-            'name': "",
-        }
-    else:
-        return {
-            'phone': phone,
-            'name': name,
-        }
