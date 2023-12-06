ee62223b9714ec2dd98b92fd662a308994c8b633
ZhangDavidXYZ <Z.David.0318@gmail.com>
2023-12-06 21:54:00 +0800

    Update render_all_purchased_items.js

--- static/js/render_all_purchased_items.js
+++ static/js/render_all_purchased_items.js
@@ -27,22 +27,6 @@ function render_data(DOM, data){
         item_div.setAttribute("class", "item-box");
         DOM.append(item_div);
 
-        // Render SPAN buyer name
-        const purchase_id_div = document.createElement("div");
-        const purchase_id = document.createElement("span");
-        purchase_id_div.setAttribute("class", "item-img-box");
-        purchase_id.setAttribute("src", item.purchase_id);
-        purchase_id_div.append(purchase_id);
-        item_div.append(purchase_id_div);
-
-        // Render SPAN buyer name
-        const buyer_name_div = document.createElement("div");
-        const buyer_name = document.createElement("span");
-        buyer_name_div.setAttribute("class", "buyer-name-box");
-        buyer_name.innerText = item.buyer_name;
-        buyer_name_div.append(buyer_name);
-        item_div.append(buyer_name_div);
-
         // Render IMG image
         const item_img_div = document.createElement("div");
         const item_img = document.createElement("img");
@@ -53,6 +37,27 @@ function render_data(DOM, data){
         item_img_div.append(item_img);
         item_div.append(item_img_div);
 
+        // Render DIV info container
+        const item_info_div = document.createElement("div")
+        item_info_div.setAttribute("class", "item-info-box")
+        item_div.append(item_info_div)
+
+        // Render SPAN buyer name
+        const purchase_id_div = document.createElement("div");
+        const purchase_id = document.createElement("span");
+        purchase_id_div.setAttribute("class", "item-img-box");
+        purchase_id.setAttribute("src", item.purchase_id);
+        purchase_id_div.append(purchase_id);
+        item_info_div.append(purchase_id_div);
+
+        // Render SPAN buyer name
+        const buyer_name_div = document.createElement("div");
+        const buyer_name = document.createElement("span");
+        buyer_name_div.setAttribute("class", "buyer-name-box");
+        buyer_name.innerText = item.buyer_name;
+        buyer_name_div.append(buyer_name);
+        item_info_div.append(buyer_name_div);
+
         // Render SPAN item name
         const item_name_div = document.createElement("div");
         const item_name = document.createElement("span");
@@ -61,7 +66,7 @@ function render_data(DOM, data){
         item_name.innerText = item.product_name;
 
         item_name_div.append(item_name);
-        item_div.append(item_name_div);
+        item_info_div.append(item_name_div);
 
         // Render SPAN item quantity
         const item_quantity_div = document.createElement("div");
@@ -71,7 +76,7 @@ function render_data(DOM, data){
         item_quantity.innerText = item.purchase_quantity;
 
         item_quantity_div.append(item_quantity);
-        item_div.append(item_quantity_div);
+        item_info_div.append(item_quantity_div);
 
         // Render SPAN purchased time
         const item_time_div = document.createElement("div");
@@ -81,7 +86,7 @@ function render_data(DOM, data){
         item_time.innerText = item.purchase_time;
 
         item_time_div.append(item_time);
-        item_div.append(item_time_div);
+        item_info_div.append(item_time_div);
 
         // Render SPAN total price
         const item_price_div = document.createElement("div");
@@ -91,6 +96,6 @@ function render_data(DOM, data){
         item_price.innerText = item.total_price;
 
         item_price_div.append(item_price);
-        item_div.append(item_price_div);
+        item_info_div.append(item_price_div);
     }
 }
