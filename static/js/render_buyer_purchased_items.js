c55bfda6bc0ece22a312325e090fd4b602bf5b12
ZhangDavidXYZ <Z.David.0318@gmail.com>
2023-12-06 21:15:54 +0800

    debug

--- static/js/render_buyer_purchased_items.js
+++ static/js/render_buyer_purchased_items.js
@@ -1,11 +1,12 @@
 /**
- * This method will render data in "purchased_item_box"
- * @data data
+ *
+ * @param DOM
+ * @param data
  */
-function render_purchased_items(data){
+function render_buyer_purchased_items(DOM, data){
 
     //clear DOM
-    purchased_item_box.empty();
+    DOM.empty();
 
     console.log("data length: " + data.length)
     for (let i = 0; i<data.length; i++) {
@@ -15,7 +16,7 @@ function render_purchased_items(data){
         // Render DIV item container
         const item_div = document.createElement("div");
         item_div.setAttribute("class", "item-box");
-        purchased_item_box.append(item_div);
+        DOM.append(item_div);
 
         const item_img_div = document.createElement("div");
         const item_img = document.createElement("img");
