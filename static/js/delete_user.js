713c620f57504befd308332c038c5a1bc5535eb2
ZhangDavidXYZ <Z.David.0318@gmail.com>
2023-12-06 18:17:42 +0800

    update

--- static/js/delete_user.js
+++ static/js/delete_user.js
@@ -1,12 +1,16 @@
 
 function delete_user(phone){
+    if (!confirm("Delete?")){
+        return;
+    }
+    
     $.ajax({
-        url:"administer/",
+        url:"administer/deleteUser",
         type:"get",
         data:{phone: phone},
         success: function (data){
             alert(data.message)
-            window.location.replace("/shop")
+            logout()
         }
     })
 }
