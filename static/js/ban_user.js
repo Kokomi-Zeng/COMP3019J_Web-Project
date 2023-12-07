45fbd6058bceb0b8186aa7d551a2e85dc359edae
ZhangDavidXYZ <Z.David.0318@gmail.com>
2023-12-07 14:58:26 +0800

    comments

--- static/js/ban_user.js
+++ static/js/ban_user.js
@@ -1,4 +1,8 @@
-
+/**
+ * This function is to ban user depend on admin phone & user phone
+ * @param admin_phone is admin phone
+ * @param user_phone is user phone
+ */
 function ban_user(admin_phone, user_phone){
     $.ajax({
         url:"/administer/banUser",
@@ -9,6 +13,8 @@ function ban_user(admin_phone, user_phone){
         },
         success:function (data){
             console.log(data.message)
+
+            // reload user info
             render_user_info(type, phone)
         }
     })
